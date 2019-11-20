bba95b9957fb0a7266f2e1e136e5a340f20aecd4
crazy-oung <d_yng@naver.com>
2019-11-20 22:03:46 +0900

    스택, 큐 구현 프로그래머스 알고리즘 문제 3번 풀이

--- _Java/Algorithm/src/algorithm/hdy/ex/Fruit.java
+++ _Java/Algorithm/src/algorithm/hdy/ex/Fruit.java
@@ -1,8 +1,12 @@
 package algorithm.hdy.ex;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class Fruit
+// 1. 자연 정렬  implements Comparable<Fruit>
+
+// 2. 사용자 임의 정렬 Comparator<T> 익명 클래스 구현 
+public class Fruit implements Comparable<Fruit>
 {
     public static void main(String[] args)
     {
@@ -13,8 +17,11 @@ public class Fruit
         fruits[2] = new Fruit("Orange", 80);
         fruits[3] = new Fruit("Banana", 90);
         
-        Arrays.sort(fruits);
+        Arrays.sort(fruits, nameCamp);
+        System.out.println(Arrays.toString(fruits));
         
+        Arrays.sort(fruits, quanCamp);
+        System.out.println(Arrays.toString(fruits));
     }
     
     public String name;
@@ -35,4 +42,28 @@ public class Fruit
     {
         return "[ 과일이름 : " + this.name + " , 잔여 수량 : " + this.quantity + " ]";
     }
+
+	@Override
+	public int compareTo(Fruit fruit) {
+		return name.compareTo(fruit.name);
+	}
+    
+    static Comparator<Fruit> nameCamp = new Comparator<Fruit>() {
+
+		@Override
+		public int compare(Fruit o1, Fruit o2) {
+			return o1.name.compareTo(o2.name);
+		}
+    	
+	};
+	
+	static Comparator<Fruit> quanCamp = new Comparator<Fruit>() {
+
+		@Override
+		public int compare(Fruit o1, Fruit o2) {
+			return o1.quantity - o2.quantity;
+		}
+    	
+	};
+    
 }
